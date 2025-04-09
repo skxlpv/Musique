d2d02f68a71a0692e1a80ef611280ba61d9dcadb
skxlpv <olexandertym0000@gmail.com>
2025-04-09 15:17:41 +0300

    refactor: update musician page image and audio display styles

---
+++ frontend/src/features/pages/pages/MusicianPage/MusicianPage.jsx
@@ -0,0 +1,22 @@
+import { UniversalFileRenderer } from "../../../dictionary/components/BrowseDocuments/UniversalFileRenderer.jsx"
+
+export const MusicianPage = () => {
+    const handleFileClick = (file) => {
+        window.location.href = `/visual_art/${file.id}`;
+    };
+
+    return(
+        <div className="w-full">
+            <div className="ml-4 mb-4">
+                <h1 className="heading-1">Musician Page</h1>
+                <h1 className="heading-3 text-neutral-500">Tune in, stand out</h1>
+            </div>
+            <div>
+                <UniversalFileRenderer
+                    category="music"
+                    onFileClick={(file) => handleFileClick(file)}
+                />
+            </div>
+        </div>
+    )
+}
